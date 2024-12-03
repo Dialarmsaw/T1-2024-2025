d52f912ac131f238480f4b0e0595533459ac8cb5
Dialarmsaw <83833689+Dialarmsaw@users.noreply.github.com>
2024-12-03 11:21:35 -0500

    Add files via upload

--- aAllScripts.py
+++ aAllScripts.py
@@ -1,21 +1,17 @@
 import os
 import pygame
-
-#Code to import
-
-import Eva
-import Evan
-import Quentin
+import importlib
+import time
 
 def main():
-    print("\n\n\nHello! This is a compilation of all of the code made throughout the trimester, sorted by person.")
-    print("In order to select a person to see their code, type in the number corrisponding to them, and hit RETURN.\n")
-    while True:
-        p = input("Input the name of the student you would like to see, with the first letter capitalized (ex. Rey not rey) >>> ")
-        try:
-            func = __import__(p)
-            func.main()
-            break
-        except:
-            print("Something failed, either the formatting was bad, the name was misspelled, or that isn't a student. Please try again.")
+	while True:
+		 print("\nHello! This is a compilation of all of the code made throughout the trimester, sorted by person.")
+		 print("In order to select a person to see their code, type in the number corrisponding to them, and hit RETURN.\n")
+		 p = input("Input the name of the student you would like to see, with the first letter capitalized (ex. Rey not rey) >>> ")
+		 os.system('clear')
+		 func = importlib.import_module(p)
+		 func.main()
+		 input("\n\n\npress RETURN to go back to the beginning")
+		 os.system('clear')
+
 main()
